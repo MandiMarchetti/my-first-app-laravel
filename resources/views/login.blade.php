51f8ca118f9b448a39b3fc1605269d7c91bd9eb4
Mandi Thomaz <118818675+MandiMarchetti@users.noreply.github.com>
2024-05-16 16:01:18 +0200

    Including the Login page, doc wepack.mix.js with the structure to include CSS file also on public folder

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="{{ asset('../css/login.css') }}">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.9/css/unicons.css">
     <title>Login</title>
 </head>
